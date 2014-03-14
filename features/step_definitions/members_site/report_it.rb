ea59bb84ffd60f73214ea906d444efe585c378c6
cosminciocan <cosmin.ciocan@freshbyteinc.com>
2014-03-14 14:26:35 +0200

    Project Updated

--- features/step_definitions/members_site/report_it.rb
+++ features/step_definitions/members_site/report_it.rb
@@ -1,49 +1,11 @@
 When(/^I navigate to the Report It page$/) do
-  visit(ReportItPage).roads_and_pavements
-  @street_name = "Abbey Gardens"
-  @street_details = "TEST DETAILS"
-  @describe_problem = "TEST PROBLEM DESCRIBED"
+  visit(ReportItPage)
+
 end
 
 When(/^I submit a Roads and pavements report$/) do
-  puts "Select Damage to road"
-  on_page(ReportItPage).damage_to_road_element.click
-  sleep 3
-  puts "Select still report the issue"
-  @current_page.still_report_issue
-  puts "Enter street name"
-  @current_page.address_of_problem_street = @street_name
-  puts "Enter street details"
-  @current_page.address_of_problem_details = @street_details
-  puts "Enter probmel descriptiuon"
-  @current_page.describe_problem = @describe_problem
-  puts "Press next button"
-  @current_page.next_button
-  sleep 1
-  puts "Scroll the page up"
-  on_page(ReportItPage).browser.div(:class => 'sp-head-wrap').send_keys :home
-  sleep 1
-  puts "Select Inside lane"
-  @current_page.inside_lane_element.click
-  puts "selecting preferred contact"
-  @current_page.preferred_contact_none_element.click
-  @first_name = "First Name Test"
-  @last_name = "Last Name Test"
-  @your_address = "Address Test"
-  @city_town = "City Test"
-  @postal_code = "Postal Code Test"
-  @email = "email@example.com"
-  if on_page(ReportItPage).browser.text.include?("First Name")
-   puts "If action triggered"
-    @current_page.first_name = @first_name
-    @current_page.last_name = @last_name
-    @current_page.your_address = @your_address
-    @current_page.city_town = @city_town
-    @current_page.postal_code = @postal_code
-    @current_page.email = @email
-  end
-  puts "Submit button"
-  @current_page.submit_button
+  on_page(ReportItPage).roads_and_pavements
+  on_page(ReportItPage).submit_damage_to_roads
 
 end
 
--- features/support/pages/members_site/report_it_page.rb
+++ features/support/pages/members_site/report_it_page.rb
@@ -20,8 +20,57 @@ class ReportItPage
   text_field(:city_town, :id => 'edit-submitted-your-details-your-details-wrapper-city-town')
   text_field(:postal_code, :id => 'edit-submitted-your-details-your-details-wrapper-postal-code')
   text_field(:email, :id => 'edit-submitted-your-details-your-details-wrapper-e-mail')
+  text_field(:phone, :id => 'edit-submitted-your-details-your-details-wrapper-phone')
 
   button(:next_button, :class => 'button next fullw-mobile ')
-  button(:submit_button, :id => 'edit-submit')
+  button(:submit_report, :id => 'edit-submit')
+
+
+  def submit_damage_to_roads
+    @street_name = "Abbey Gardens"
+    @street_details = "TEST DETAILS"
+    @describe_problem = "TEST PROBLEM DESCRIBED"
+    @first_name = "First Name Test"
+    @last_name = "Last Name Test"
+    @your_address = "Address Test"
+    @city_town = "City Test"
+    @postal_code = "Postal Code Test"
+    @email = "email@example.com"
+    @phone = "01234567890"
+
+#    browser.wait_until(self.damage_to_road?)
+    puts "Select Damage to road"
+    self.damage_to_road_element.click
+    puts "Select still report the issue"
+    self.still_report_issue
+    puts "Enter street name"
+    self.address_of_problem_street = @street_name
+    puts "Enter street details"
+    self.address_of_problem_details = @street_details
+    puts "Enter probmel descriptiuon"
+    self.describe_problem = @describe_problem
+    puts "Press next button"
+    self.next_button
+    sleep 1
+    puts "Scroll the page up"
+    browser.div(:class => 'sp-head-wrap').send_keys :home
+    sleep 1
+    puts "Select Inside lane"
+    self.inside_lane_element.click
+    puts "selecting preferred contact"
+    self.preferred_contact_none_element.click
+    if browser.text.include?("First Name")
+      puts "If action triggered"
+      self.first_name = @first_name
+      self.last_name = @last_name
+      self.your_address = @your_address
+      self.city_town = @city_town
+      self.postal_code = @postal_code
+      self.email = @email
+      self.phone = @phone
+    end
+    puts "Submit button"
+    self.submit_report
+  end
 
 end
